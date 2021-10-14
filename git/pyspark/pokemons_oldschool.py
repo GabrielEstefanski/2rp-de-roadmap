fa2bf503fdc3cc7d504cdfabfdc0a7f26d7c33ca
Gabriel Braga Estefanski <gbraga2004@outlook.com>
2021-10-14 16:27:35 -0300

    pokemons_oldschool.py

--- git/pyspark/pokemons_oldschool.py
+++ git/pyspark/pokemons_oldschool.py
@@ -6,19 +6,20 @@ from pyspark.sql import functions as func
 base = spark.table("work_dataeng.generation_gabriel_braga")
 
 
-base = base.select(["date_introduced","generation"])
+base = base.select(["generation","data_introduced"])
 
-base = base.filter("date_introduced<'2002-11-21'")
+base = base.filter("data_introduced<'2002-11-21'")
 
 
 base2 = spark.table("work_dataeng.pokemon_gabriel_braga")
-
 base2 = base2.select(["generation","idnum", "name", "hp", "speed", "attack", "special_attack", "defense", "special_defense"])
+base= base.withColumnRenamed('generation', 'pokememon_generation')
 
+base = base.cache()
 
 result = base.join(base2,
-base.generation == base2.generation, 'inner')
+base.pokememon_generation == base2.generation, 'inner')
 
-result.show()
+
 
 result.write.insertInto("work_dataeng.pokemons_oldschool_gabriel_braga")
